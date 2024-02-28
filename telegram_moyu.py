e0e78cd4ae759ba220f1de3ec636b15f0f895c71
Chris Shao <christophershao0@gmail.com>
2024-02-28 15:02:05 -0500

    remove 2fa and SMS code requirement

--- telegram_moyu.py
+++ telegram_moyu.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb  8 15:37:38 2023
+Created on Wed Feb 8 15:37:38 2023
 
 @author: chris
 """
 
 import logging
-import getpass
 from telethon import TelegramClient
 from telethon.tl.functions.account import UpdateStatusRequest
 import time
@@ -14,29 +13,22 @@ import time
 # API Information
 api_id = 20294799
 api_hash = 'f4cd0bb899a3a774cfa6fea181f78bc3'
-delay = 15  # Second
+delay = 15  # Seconds
 
 # Logging Configuration
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, filename='telegram_bot.log')
 
-# Login to Telegram
-logging.info("Trying to Login to Telegram...")
+# Initialize Telegram Client
 client = TelegramClient('session_file', api_id, api_hash)
-client.connect()
-if client.is_user_authorized() is not True:
-    logging.info('You have not login yet, Trying to log you in...')
-    logging.info('if you have 2FA password, please enter right now. This Password will not be stored')
-    password = getpass.getpass()
-    if password != '':
-        client.start(password=password)
-    else:
-        client.start()
 
-if client.is_user_authorized():
+async def main():
+    await client.start()
     logging.info("You are now AlwaysOnline™, Yah!")
+    
     while True:
-        client(UpdateStatusRequest(offline=False))
+        await client(UpdateStatusRequest(offline=False))
+        logging.debug("Updated status to online. Sleeping for {} seconds...".format(delay))
         time.sleep(delay)
-        logging.debug("Sleep for 1 min")
-else:
-    logging.fatal("Login Fails, please retry... 失败，请重试！")
+
+with client:
+    client.loop.run_until_complete(main())
